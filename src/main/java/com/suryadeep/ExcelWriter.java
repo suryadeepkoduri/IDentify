f68448b09e99f8413e86e036aa85a61c8a144694
suryadeepkoduri <suryadeepkoduri@gmail.com>
2024-02-28 11:52:57 +0530

    added functionality in ExcelWriter.java

--- src/main/java/com/suryadeep/ExcelWriter.java
+++ src/main/java/com/suryadeep/ExcelWriter.java
@@ -1,6 +1,57 @@
 package com.suryadeep;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.io.File;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelWriter {
 
+    String getDate() {
+        LocalDate currenDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = currenDate.format(formatter);
+
+        return formattedDate;
+    }
+
+    // Method to create a new Excel Workbook by given name
+    @SuppressWarnings("resource")
+    void createExcelWorkbook(String name) throws FileNotFoundException, IOException {
+        name = addXLSXExtension(name);
+
+        File f = new File(name);
+        if (f.exists()) {
+            return;
+        }
+
+        XSSFWorkbook wb = new XSSFWorkbook();
+        try (FileOutputStream fileOut = new FileOutputStream(name)) {
+            wb.write(fileOut);
+        }
+    }
+
+    //Method to create a Excel Worksheet in Excel Workbook
+    void createExcelWorksheet(String name) {
+        name = addXLSXExtension(name);
+    }
+
+    String addXLSXExtension(String name) {
+        if (FilenameUtils.getExtension(name)!=".xlsx") {
+            name = name+".xlsx";
+            return name;
+        }else {
+            return name;
+        }
+    }
+
+    public ExcelWriter() {
+
+    }
 
 }
--- src/main/java/com/suryadeep/Main.java
+++ src/main/java/com/suryadeep/Main.java
@@ -1,7 +1,22 @@
 package com.suryadeep;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ExcelWriter ew = new ExcelWriter();
+
+        System.out.println(ew.getDate());
+        try {
+            ew.createExcelWorkbook("surya");
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
